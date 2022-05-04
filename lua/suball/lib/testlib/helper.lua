5957c7493d4be70084a49d6839a6b4094d96a213
notomo <notomo.motono@gmail.com>
2022-05-04 15:26:29 +0900

    Use test helper.cleanup()

--- lua/suball/lib/testlib/helper.lua
+++ lua/suball/lib/testlib/helper.lua
@@ -1,23 +1,16 @@
 local plugin_name = vim.split((...):gsub("%.", "/"), "/", true)[1]
-local M = require("vusted.helper")
+local helper = require("vusted.helper")
 
-M.root = M.find_plugin_root(plugin_name)
+helper.root = helper.find_plugin_root(plugin_name)
 
-function M.before_each()
-  vim.cmd("filetype on")
-  vim.cmd("syntax enable")
+function helper.before_each() end
+
+function helper.after_each()
+  helper.cleanup()
+  helper.cleanup_loaded_modules(plugin_name)
 end
 
-function M.after_each()
-  vim.cmd("tabedit")
-  vim.cmd("tabonly!")
-  vim.cmd("silent! %bwipeout!")
-  vim.cmd("filetype off")
-  vim.cmd("syntax off")
-  M.cleanup_loaded_modules(plugin_name)
-end
-
-function M.set_lines(lines)
+function helper.set_lines(lines)
   vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(lines, "\n"))
 end
 
@@ -27,4 +20,4 @@ asserts.create("lines"):register_eq(function()
   return table.concat(vim.fn.getbufline("%", 0, "$"), "\n")
 end)
 
-return M
+return helper
