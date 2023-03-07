8740cba71203eac623bdbabdcaae8a44a09f7694
notomo <notomo.motono@gmail.com>
2023-03-07 23:02:00 +0900

    Adjust @return doc

--- lua/suball/init.lua
+++ lua/suball/init.lua
@@ -3,7 +3,7 @@ local M = {}
 --- Returns a substitute command for expr mapping.
 --- @param before string: a word before substitution
 --- @param after string: a word after substitution
---- @return string: a substitute command expr
+--- @return string # a substitute command expr
 function M.map(before, after)
   return require("suball.command").map(before, after)
 end
